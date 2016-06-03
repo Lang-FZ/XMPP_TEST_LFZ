6601e62896578c4f32d3e7e67309e0cd01fbde17
Lang-FZ <446003664@qq.com>
2016-06-04 02:30:04 +0800

    Update AppDelegate.h

--- XMPP_TEST_LFZ1/AppDelegate.h
+++ XMPP_TEST_LFZ1/AppDelegate.h
@@ -15,15 +15,3 @@
 
 
 @end
-
-//# XMPP_TEST_LFZ
-//
-//# 仿写了战旗TV的部分功能
-//
-//-有点感兴趣就找资料学了下视频直播，抓包不太熟练找不到战旗的聊天群就自己集成了环信的XMPP
-//
-//-弹幕用的https://github.com/unash/BarrageRenderer
-//
-//-播放器用的https://github.com/ShelinShelin/XLVideoPlayer
-//
-//#前排找工作，实习岗也请骚扰我:smiley:
